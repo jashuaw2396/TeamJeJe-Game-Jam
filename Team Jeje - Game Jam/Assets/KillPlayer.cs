00bca4cb95853e1bb9e87adba02a0745beeec77f
sonyssamurai <43693545+sonyssamurai@users.noreply.github.com>
2018-09-29 12:48:25 -0700

    Created killobject, script and prefab

---
+++ Team Jeje - Game Jam/Assets/KillPlayer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KillPlayer : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.layer == 9)
+        {
+            GameObject.FindGameObjectWithTag("PlayerTracker").GetComponent<PlayerTracker>().m_isAlive = false;
+        }
+    }
+}
