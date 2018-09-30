9ef977d740baf815c89c779e97cc1543d9a501e6
sonyssamurai <43693545+sonyssamurai@users.noreply.github.com>
2018-09-29 20:14:36 -0700

    Door, Platform, Wall with scrips
    added them and logic

---
+++ Team Jeje - Game Jam/Assets/Door.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour {
+
+    public bool open = false;
+    private bool wasOpen = false;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (open)
+        {
+            if (!wasOpen)
+            {
+                OpenDoor();
+                wasOpen = true;
+            }
+        }
+        else
+        {
+            if (wasOpen)
+            {
+                OpenDoor();
+                wasOpen = false;
+            }
+        }
+	}
+
+    void OpenDoor()
+    {
+        if (open)
+        {
+            GetComponent<BoxCollider2D>().enabled = false;
+        }
+        else
+        {
+            GetComponent<BoxCollider2D>().enabled = true;
+        }
+    }
+}
---
+++ Team Jeje - Game Jam/Assets/Platform.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Platform : MonoBehaviour {
+
+    public bool on = false;
+    private bool wasOn = false;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (on)
+        {
+            if (!wasOn)
+            {
+                ChangeStatePlatform();
+                wasOn = true;
+            }
+        }
+        else
+        {
+            if (wasOn)
+            {
+                ChangeStatePlatform();
+                wasOn = false;
+            }
+        }
+    }
+
+    void ChangeStatePlatform()
+    {
+        if (on)
+        {
+            BoxCollider2D[] coliders = GetComponents<BoxCollider2D>();
+
+            for(int i=0; i < coliders.Length;++i)
+            {
+                coliders[i].enabled = false;
+            }
+        }
+        else
+        {
+            BoxCollider2D[] coliders = GetComponents<BoxCollider2D>();
+
+            for (int i = 0; i < coliders.Length; ++i)
+            {
+                coliders[i].enabled = true;
+            }
+        }
+    }
+}
